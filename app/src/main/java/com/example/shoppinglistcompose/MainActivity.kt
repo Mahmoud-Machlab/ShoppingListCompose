24f2145925653b3680e65c596888e296a37c6b49
Frank <f.neumann.berlin@googlemail.com>
2023-04-28 09:22:24 +0200

    Mit AppBar

--- app/src/main/java/com/example/shoppinglistcompose/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglistcompose/MainActivity.kt
@@ -3,13 +3,28 @@ package com.example.shoppinglistcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.BottomAppBar
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.shoppinglistcompose.ui.theme.ShoppingListComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,9 +35,9 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = MaterialTheme.colors.background
                 ) {
-
+                    FullScreen()
                 }
             }
         }
@@ -30,11 +45,62 @@ class MainActivity : ComponentActivity() {
 }
 
 
+@Composable
+fun FullScreen(){
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        "ShoppingList",
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
+                    )
+                },
+                navigationIcon = {
+                    IconButton(onClick = { /* doSomething() */ }) {
+                        Icon(
+                            imageVector = Icons.Filled.Menu,
+                            contentDescription = "Localized description"
+                        )
+                    }
+                },
+                actions = {
+                    IconButton(onClick = { /* doSomething() */ }) {
+                        Icon(
+                            imageVector = Icons.Filled.Delete,
+                            contentDescription = "Localized description"
+                        )
+                    }
+                }
+            )
+        },
+//        content = { innerPadding ->
+//            LazyColumn(
+//                contentPadding = innerPadding,
+//                verticalArrangement = Arrangement.spacedBy(8.dp)
+//            ) {
+//                val list = (0..75).map { it.toString() }
+//                items(count = list.size) {
+//                    Text(
+//                        text = list[it],
+//                        style = MaterialTheme.typography.h2,
+//                        modifier = Modifier
+//                            .fillMaxWidth()
+//                            .padding(horizontal = 16.dp)
+//                    )
+//                }
+//            }
+//        }
+    ){
+        it.calculateBottomPadding()
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ShoppingListComposeTheme {
-
+        FullScreen()
     }
 }
