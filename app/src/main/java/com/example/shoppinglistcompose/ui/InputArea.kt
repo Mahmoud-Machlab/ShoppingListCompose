9444a1c32df438fffa7459ccc4c2c1f0d3f80a2b
Frank <f.neumann.berlin@googlemail.com>
2023-05-02 08:08:21 +0200

    Mit isChecked und Checkbox

--- app/src/main/java/com/example/shoppinglistcompose/ui/InputArea.kt
+++ app/src/main/java/com/example/shoppinglistcompose/ui/InputArea.kt
@@ -5,12 +5,15 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.MaterialTheme.colors
 import androidx.compose.material.OutlinedButton
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.internal.isLiveLiteralsEnabled
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
@@ -30,9 +33,10 @@ fun InputArea() {
         OutlinedTextField(
             value = quantity,
             onValueChange = { quantity = it },
+            shape = RoundedCornerShape(8.dp),
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
             modifier = Modifier
-                .padding(top = 16.dp, )
+                .padding(top = 8.dp)
                 .width(64.dp)
                 .weight(1.2f),
             label = { Text(text = "Anz.") }
@@ -40,22 +44,25 @@ fun InputArea() {
         OutlinedTextField(
             value = product,
             onValueChange = { product = it },
+            shape = RoundedCornerShape(8.dp),
             modifier = Modifier
-                .padding(top = 16.dp, start = 8.dp)
+                .padding(top = 8.dp, start = 4.dp)
                 .weight(3f),
             label = { Text(text = "Artikel") }
         )
 
         OutlinedButton(
             onClick = {
-                shoppingMemoViewModel.insertOrUpdate(ShoppingMemo(quantity.toInt(), product) )
+                shoppingMemoViewModel.insertOrUpdate(ShoppingMemo(quantity.toInt(), product))
                 quantity = ""
                 product = ""
             },
+            shape = RoundedCornerShape(8.dp),
+            enabled = !(quantity.isBlank() || product.isBlank()),
             modifier = Modifier
-                .padding(top = 16.dp, start = 8.dp, )
-                .height(54.dp)
-                .align(Alignment.CenterVertically),
+                .padding(top = 8.dp, start = 4.dp)
+                .height(56.dp)
+                .align(Alignment.Bottom),
 
             colors = ButtonDefaults.buttonColors()
 
--- app/src/main/java/com/example/shoppinglistcompose/ui/ShoppinList.kt
+++ app/src/main/java/com/example/shoppinglistcompose/ui/ShoppinList.kt
@@ -1,7 +1,11 @@
 package com.example.shoppinglistcompose.ui
 
 
+import android.util.Log
 import android.widget.Toast
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -11,7 +15,10 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material.Checkbox
+import androidx.compose.material.CheckboxDefaults
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
@@ -44,6 +51,7 @@ fun ListView() {
     }
 }
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun ListItem(memo: ShoppingMemo) {
     val context = LocalContext.current
@@ -52,22 +60,38 @@ fun ListItem(memo: ShoppingMemo) {
     }
 
     Row(verticalAlignment = Alignment.CenterVertically) {
-        Column(Modifier.weight(3f)) {
-            ClickableText(
-                text = buildAnnotatedString { append(memo.toString() )},
+        Column(Modifier.weight(3f)
+            .combinedClickable(
+                onLongClick = {
+                    Toast.makeText(context, "Long Click", Toast.LENGTH_SHORT).show()},
+                onDoubleClick = {
+                    Toast.makeText(context, "Double Click", Toast.LENGTH_SHORT).show()},
+            ){ Toast.makeText(context, "Click", Toast.LENGTH_SHORT).show()}
+        ) {
+            Text(
+                text = buildAnnotatedString { append(memo.toString()) },
                 style = TextStyle(
-                    color = if(isChecked) Color.LightGray else Color.DarkGray,
-                    textDecoration = if(isChecked) TextDecoration.LineThrough else TextDecoration.None,
+                    color = if (isChecked) Color(0xFFD0BCFF) else Color.DarkGray,
+                    textDecoration = if (isChecked) TextDecoration.LineThrough else TextDecoration.None,
                     fontSize = 24.sp
                 ),
-                onClick = {
-                    Toast.makeText(context, "$memo", Toast.LENGTH_SHORT).show()
-                }
-                )
+               modifier = Modifier
+                   .combinedClickable(
+                   onLongClick = {
+                       Toast.makeText(context, "Long Click", Toast.LENGTH_SHORT).show()},
+                   onDoubleClick = {
+                       Toast.makeText(context, "Double Click", Toast.LENGTH_SHORT).show()},
+               ){ Toast.makeText(context, "Click Text", Toast.LENGTH_SHORT).show()}
+            )
         }
         Column(Modifier.weight(1f)) {
             Checkbox(
-                checked = isChecked, onCheckedChange = { isChecked = it },
+                checked = isChecked, onCheckedChange = {
+                    isChecked = it
+                    memo.isSelected = it
+                    shoppingMemoViewModel.insertOrUpdate(memo)
+                },
+                colors = CheckboxDefaults.colors(MaterialTheme.colors.primary),
                 modifier = Modifier.align(Alignment.End)
             )
         }
